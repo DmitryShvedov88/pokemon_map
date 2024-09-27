6d1eb81895659b38ddf6c0aa60d3ca0f1721a6a8
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2024-09-27 20:41:26 +0300

    show Pokemon name in admin UI.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -3,3 +3,7 @@ from django.db import models  # noqa F401
 
 class Pokemon(models.Model):
     title = models.CharField(verbose_name="Name", max_length=200,)
+    def __str__(self):
+        if self.title:
+            return self.title
+        return f'{self.title} (inactive)'

af4f88613ab29cc66f3ad49e4122a1e38c5a4f79
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2024-10-02 21:06:09 +0300

    add PokemonEntity model.

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -2,9 +2,23 @@ from django.db import models  # noqa F401
 
 
 class Pokemon(models.Model):
+    """Пукемон"""
+    id = models.AutoField(auto_created=True, primary_key=True)
     title = models.CharField(verbose_name="Name", max_length=200,)
     photo = models.ImageField(upload_to='Photo', null=True)
+
     def __str__(self):
         if self.title:
             return self.title
-        return f'{self.title} (inactive)'
+        return f'{self.title} (Не существует)'
+
+class PokemonEntity(models.Model):
+    """Характеристика покемона"""
+    pokemon = models.ForeignKey(Pokemon, verbose_name="Покемон", on_delete=models.CASCADE, related_name="entities")
+    lat = models.FloatField("Широта")
+    lon = models.FloatField("Долгота")
+
+    def __str__(self):
+        if self.pokemon.title:
+            return self.pokemon.title
+        return f'{self.pokemon.title} (Не существует)'

bd43dc80b0ad3fa226596f0ecde0db462694f563
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2024-10-03 21:59:41 +0300

    show pokemons on mainpage.

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -1,6 +1,6 @@
 import folium
 import json
-
+from .models import Pokemon, PokemonEntity
 from django.http import HttpResponseNotFound
 from django.shortcuts import render
 
@@ -39,12 +39,13 @@ def show_all_pokemons(request):
                 pokemon['img_url']
             )
 
+
     pokemons_on_page = []
-    for pokemon in pokemons:
+    for pokemon in Pokemon.objects.all():
         pokemons_on_page.append({
-            'pokemon_id': pokemon['pokemon_id'],
-            'img_url': pokemon['img_url'],
-            'title_ru': pokemon['title_ru'],
+            'pokemon_id': pokemon.id,
+            'img_url': pokemon.photo,
+            'title_ru': pokemon.title,
         })
 
     return render(request, 'mainpage.html', context={
